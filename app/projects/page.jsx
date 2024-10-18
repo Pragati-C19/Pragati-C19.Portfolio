170cf011846c16562f629bc51f35abd4a1611474
Pragati Chothe <pragatichothe@gmail.com>
2024-10-18 18:51:35 +0530

    📸 added image on Projects Page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import ProjectCards from "@/components/projects/project-cards";
 import SearchProjects from "@/components/projects/project-search";
 import { useProjects } from "@/hooks/useProjects";
+import ProjectImage from "@/public/assets/my-projects.png";
+import Image from "next/image";
 
 const Projects = () => {
   const { getDisplayedProjects } = useProjects();
@@ -16,10 +18,24 @@ const Projects = () => {
   return (
     <section className="bg-gradient-to-b from-blue-100 to-pink-200 p-8">
       <div className="text-center mt-10 mb-10 ">
-        <h1 className="text-4xl sm:text-4xl mb-1 text-indigo-600 dark:text-ternary-light font-bold font-serif">
-          All Projects
-        </h1>
+        <div className="flex justify-center items-center">
+          {/* Image aligned with the heading */}
+          <h1 className="text-4xl sm:text-4xl mb-1 text-indigo-600 dark:text-ternary-light font-bold font-serif">
+            Devfest
+          </h1>
+          <Image
+            src={ProjectImage}
+            alt="Project Image"
+            width={110}
+            height={110}
+            className="ml-8 mr-8"
+          />
+          <h1 className="text-4xl sm:text-4xl mb-1 text-indigo-600 dark:text-ternary-light font-bold font-serif">
+            Carnival
+          </h1>
+        </div>
       </div>
+
       <div className="container mx-auto mb-5 sm:mb-10">
         {/* Search and Filter Projects */}
         <SearchProjects setFilteredProjects={setFilteredProjects} />
