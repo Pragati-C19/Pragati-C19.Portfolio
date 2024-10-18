905e12530e0259114cc226f66e7d0815769dbc0a
Pragati Chothe <pragatichothe@gmail.com>
2024-10-18 15:45:07 +0530

    ✅ scoller is completed

--- components/about/skills-scroller.jsx
+++ components/about/skills-scroller.jsx
@@ -1,15 +1,17 @@
+// Skills Scroller
+
+"use client"; // Adding this directive to make it a Client Component
 
 import Marquee from "react-fast-marquee";
 import { skillsData } from "@/data/about-me";
 
 const SkillsScroller = () => (
   <div className="skills-scroller my-10">
-    <h2 className="text-2xl font-semibold text-center mb-4">My Skills</h2>
     <Marquee gradient={false} speed={40}>
       {skillsData.map((skill, index) => (
         <div
           key={index}
-          className="px-4 py-2 bg-gray-100 dark:bg-gray-700 rounded-lg mx-2 text-center text-lg font-medium"
+          className="px-4 py-2 border-2 border-gray-700 hover:bg-indigo-200 dark:bg-gray-700 rounded-full mx-2 text-center text-lg text-gray-700 font-medium"
         >
           {skill}
         </div>
