7841165b6d29ead932aa0e46fac2af5f28280dec
Pragati Chothe <pragatichothe@gmail.com>
2024-10-19 23:47:27 +0530

    📱 made homepage responsive

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,12 @@ import ProjectsSection from "@/components/homepage/project-section";
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto bg-gradient-to-b from-blue-100 to-pink-200">
+    <section className="bg-gradient-to-b from-blue-100 to-pink-200">
+    <div className="container mx-auto">
       <HeroSection />
       <ProjectsSection />
     </div>
+    </section>
   );
 };
 
