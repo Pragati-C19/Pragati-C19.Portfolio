cf44a4a0753e21e7dd98aba292b510b8f2d4e6f6
Pragati Chothe <pragatichothe@gmail.com>
2024-10-19 23:47:59 +0530

    ⚡️ added label in select filter to improve performance

--- components/projects/projects-filter.jsx
+++ components/projects/projects-filter.jsx
@@ -8,7 +8,8 @@ const ProjectsFilter = ({ setSelectProject }) => {
       onChange={(e) => {
         setSelectProject(e.target.value);
       }}
-      className="font-general-medium w-full sm:w-auto px-4 sm:px-6 py-2 border dark:border-secondary-dark rounded-lg text-sm sm:text-md dark:font-medium bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light transition duration-300 ease-in-out">
+      className="font-general-medium w-full sm:w-auto px-4 sm:px-6 py-2 border dark:border-secondary-dark rounded-lg text-sm sm:text-md dark:font-medium bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light transition duration-300 ease-in-out"
+      aria-label="Choose an option">
       <option value="" className="text-sm sm:text-md">
         All Projects
       </option>
