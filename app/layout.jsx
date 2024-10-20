e35274f2bfb93b594bbea77e041a5f46d9e10201
Pragati Chothe <pragatichothe@gmail.com>
2024-10-20 15:37:47 +0530

    ⚡️ SEO Performance Boast | added structuredData json

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ import Footer from "@/components/footer";
 
 export const metadata = {
   title: "Pragati Chothe | Full Stack Developer and UI/UX Designer",
-  description: "Explore my portfolio! I’m Pragati, a Full Stack Developer and UI/UX Designer with 2 years of experience. I specialize in HTML, CSS, JavaScript, React.js, Next.js, Node.js, Express.js, MongoDB, MySQL, and Firebase. My projects include Hi-tech, DokumenHub, Snackmate, and the Pokémon-themed Dragamine game. I’m passionate about learning new technologies, currently focusing on Docker. Connect with me for exciting collaborations!",
+  description: "Explore my portfolio! I’m Pragati Chothe, a Full Stack Developer and UI/UX Designer with 2 years of experience. I specialize in HTML, CSS, JavaScript, React.js, Next.js, Node.js, Express.js, MongoDB, MySQL, and Firebase. My projects include Hi-tech, DokumenHub, Snackmate, and the Pokémon-themed Dragamine game. I’m passionate about learning new technologies, currently focusing on Docker. Connect with me for exciting collaborations!",
   keywords:
     "Freelancer, Full Stack Developer, UI Designer, Web Developer, UX Designer, Frontend Developer, Backend Developer, MERN Stack Developer, Responsive Web Designer",
   openGraph: {
@@ -26,14 +26,18 @@ export const viewport = {
   initialScale: 1,
 };
 
-const structuredData = {
+// TODO: Need to check the testing if it's working or need to add <head><script type="application/ld+json"> {JSON.stringify(structuredData)} </script></head>
+export const structuredData = {
   "@context": "https://schema.org",
   "@type": "Person",
-  name: "Pragati Chothe",
-  url: "https://pragatichothe.in/", // live URL
-  image: "https://pragatichothe.in/profile-image.png", // Your image URL
-  description: "Explore my portfolio! I’m Pragati, a Full Stack Developer and UI/UX Designer with 2 years of experience. I specialize in HTML, CSS, JavaScript, React.js, Next.js, Node.js, Express.js, MongoDB, MySQL, and Firebase. My projects include Hi-tech, DokumenHub, Snackmate, and the Pokémon-themed Dragamine game. I’m passionate about learning new technologies, currently focusing on Docker. Connect with me for exciting collaborations!",
-  sameAs: [
+  "name": "Pragati Chothe",
+  "alternateName": ["Pragati_C19", "pragati-c19", "Pragati", "Pragati-C19", "Chothe", "PC19", "Pragati Chothe Projects", "Pragati Chothe Portfolio"],
+  "jobTitle": ["Freelancer", "Full Stack Developer", "UI/UX Designer"],
+  "url": "https://pragatichothe.in/", 
+  "image": "https://pragatichothe.in/profile-image.png", 
+  "email": "mailto:pragatichothe@gmail.com",
+  "description": "Explore my portfolio! I’m Pragati Chothe, a Full Stack Developer and UI/UX Designer with 2 years of experience. I specialize in HTML, CSS, JavaScript, React.js, Next.js, Node.js, Express.js, MongoDB, MySQL, and Firebase. My projects include Hi-tech, DokumenHub, Snackmate, and the Pokémon-themed Dragamine game. I’m passionate about learning new technologies, currently focusing on Docker. Connect with me for exciting collaborations!",
+  "sameAs": [
     "https://linkedin.com/in/pragati-c19",
     "https://github.com/Pragati-C19",
     "https://dribbble.com/pragati_c19",
@@ -43,9 +47,28 @@ const structuredData = {
     "https://www.chess.com/member/pragati_c19",
     "https://www.hackerrank.com/profile/pragati_c19",
     "https://leetcode.com/u/pragati__c19/"
-    // Add more social links if needed
   ],
+  "knowsAbout": [
+    "JavaScript", "React.js", "Next.js", "Node.js", "Express.js", "Tailwind CSS", 
+    "MongoDB", "MySQL", "Firebase", "UI/UX Design", "Figma", "HTML", "CSS", 
+    "Redux", "REST APIs", "Git", "SQL Database Design", "Data Structures", 
+    "Algorithms", "jQuery UI", "Release Notes", "Amazon Route 53", "Netlify", 
+    "Secure Authentication", "Notion Productivity Software", "Problem Solving", 
+    "Mobile User Experience", "Back-End Web Development", "Client Side Scripting", 
+    "Agile Web Development", "Software Development Life Cycle (SDLC)", "JWT", 
+    "Python", "C++", "Graphic Design"
+  ],
+  "award": {
+    "@type": "CreativeWork",
+    "name": "Toycathon 2021 Grand Finalist",
+    "issuer": {
+      "@type": "Organization",
+      "name": "MHRD's Ministry"
+    },
+    "datePublished": "2021-06"
+  }
 };
+
 
 export default function RootLayout({ children }) {
   return (
