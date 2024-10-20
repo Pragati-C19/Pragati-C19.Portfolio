d09fc992d5220e0dcc315abe774c9ac5e0a56255
Pragati Chothe <pragatichothe@gmail.com>
2024-10-20 15:38:35 +0530

    ⚡️ SEO Performance Boast | Sitemap is added and working

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://pragatichothe.in', // Your live site URL
+    generateRobotsTxt: true, // This will generate a robots.txt file
+    exclude: ['/404'], // Exclude specific routes from the sitemap
+    changefreq: 'weekly', // Optional
+    priority: 0.7, // Optional
+  };
+  
