cca3075a2b2881a88d3d2eae420083d0a89edd15
Pragati Chothe <pragatichothe@gmail.com>
2024-11-15 19:02:18 +0530

    ✅ Deployed the portfolio successfully

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@
 
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  output: 'export',
+  images: {
+    unoptimized: true, // Required if you use Next.js Image component for static export
+  },
 };
 
 export default nextConfig;
